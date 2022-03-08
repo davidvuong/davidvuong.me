71f60302fcee85a12480bfc9ba705cc1dc2e5e8e
David Vuong <david.vuong256@gmail.com>
2022-03-08 11:35:39 +1100

    copy: description

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ export const App = () => (
               I'm David. It's nice to meet you.
             </h1>
             <p className="mt-2 max-w-lg mx-auto text-medium text-gray-300 sm:text-lg md:mt-4 md:text-lg lg:max-w-3/4">
-              degen, defi investor, inventor, blockchain, software engineer.
+              software engineer, defi investor, inventor, degen, blockchain
             </p>
             <div className="mt-8 max-w-sm mx-auto flex space-x-6 items-center content-center justify-center">
               <a href="https://github.com/davidvuong">
